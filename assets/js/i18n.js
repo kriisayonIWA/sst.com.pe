69b6cb7d98593cdbf38b148a29cf1102999cd763
kriisayon/Iwa <caromerov@unac.edu.pe>
2023-04-09 18:17:50 -0500

    Agregacion de la funcionalidad de multilenguaje

---
+++ assets/js/i18n.js
@@ -0,0 +1,274 @@
+var resources = {
+    en: {
+      translation: {
+        home: 'Home',
+        services: 'What we do?',
+        about: 'Who we are?',
+        blog:'Current technology',
+        contact: 'Contact',
+        language: 'Language',
+        es: 'Español',
+        en: 'English',
+        herotitle: 'Building the future of your company',
+        herodescription: 'Together, we will transform your vision of the future into reality by harnessing the benefits of the digital world',
+        herobutton:'Discover',
+        dotitle:'What we do?',
+        dosubtitle:'We transform your company through technology',
+        dodescription:'Our services allow us to face the challenge of digital transformation, from the design and planning of the business to the consolidation of sales growth, reduction of operating and storage costs.',
+        dobutton:'All our services',
+        differencetitle:'OUR DIFFERENCE',
+        d1:'Human talent in Technology',
+        d2:'Custom Software',
+        d3:'Speed and precision: Artificial Intelligence',
+        d4:'Build the most economical and reliable infrastructure',
+        d5:'The security of being in the clouds',
+        d6:'Networks unite the world in one place',
+        wetitle:'Who we are?',
+        wesubtitle:'We develop innovative technological solutions through our knowledge and experience',
+        wedescription:'SST is a company specialized in services, consulting and IT-based business solutions with more than 10 years of experience in the industry, accompanying the transformation and growth of leading companies through technology.',
+        webutton:'Come and meet us!',
+        btitle:'Stay informed',
+        benlace1:'Web development',
+        btitle1:'The importance of human talent and software development',
+        benlace2:'Digital Transformation',
+        btitle2:'The importance of digital transformation in PYMES',
+        benlace3:'Business Intelligence',
+        btitle3:'How business intelligence solutions are helping companies make informed decisions',
+        fdescription:'We are efficient and capable of facing technological challenges, providing a comprehensive quality service',
+        flinks:'Solutions',
+        flink1:'Infrastructure',
+        flink2:'Software',
+        flink3:'Consulting',
+        flink4:'Digital Marketing',
+        fcontact:'Contact',
+        fmail:'Mails:',
+        fphone:'Phone:',
+        fsub:'Stay informed',
+        fsubdescription:'Sign up to receive our newsletter by email and get the latest news delivered straight to your inbox.',
+        fsubbutton:'Subscribe',
+        contacthero:'Join the transformation and make a difference today!',
+        cspan1: 'Call us',
+        cdes1: 'We are here to help you at all times.',
+        cspan2: 'Locate us',
+        cdes2: 'View location',
+        cspan3: 'Send us an email',
+        cdes3: 'Our team of experts is at your disposal.',
+        formtitle: 'Get in touch',
+        formsub: 'Talk to an expert',
+        formbutton: 'Send!',
+        csocials: 'Follow us on our social networks',
+        dotitle2: "Innovation, the engine of the future",
+        dodes2: "In SST, trust is essential for success. We offer comprehensive technological solutions and build trusting relationships with our clients and their communities to achieve a positive impact on their businesses and the world.",
+        servicestitle: "Services, technology for your business",
+        productstitle: "Products. Solutions to reach the next level",
+        erpcomercial: "Trace ERP Commercial",
+        comercialdes: "Optimize your company's management with Trace ERP, the integrated computer system that simplifies and unifies all areas of your business on a single platform. Make informed and effective decisions, optimize your processes, and take your operations to the next level.",
+        productsbutton: "See more",
+        erpindustrial: "Trace ERP Industrial",
+        industrialdes: "Trace ERP Industrial is SST's enterprise resource planning (ERP) computer solution designed for industrial sector companies. With it, you can integrate all areas of your business into a single system, allowing you to improve efficiency, reduce costs, and make more informed decisions.",
+        erphotel: "Trace ERP Hotels",
+        hoteldes: "Optimize your company's management with Trace ERP, the integrated computer system that simplifies and unifies all areas of your business on a single platform. Make informed and effective decisions, optimize your processes, and take your operations to the next level.",
+        erprestaurant: "Trace ERP Restaurant",
+        restaurantdes: "Trace ERP Restaurant is SST's enterprise resource planning (ERP) computer solution designed for restaurant sector companies. With it, you can integrate all areas of your business into a single system, allowing you to improve efficiency, reduce costs, and make more informed decisions.",
+        usuariotitle: "Trust in action",
+        uspan1: "Artificial Intelligence",
+        topic1: "Automate tasks with AI",
+        usuariodes: "Case study",
+        uspan2: "Internet of Things",
+        topic2: "Internet of Things: incorporating data into everyday life",
+        uspan3: "Structured Cabling",
+        topic3: "Peru's cabling",
+        wespan: "Building on trust",
+        wedesmain: "We are a responsible company, generating positive changes for the benefit of society",
+        wechar: "Confidence,"+"<br>"+
+        "Experience and"+"<br>"+
+        "Innovation",
+        wechardes: "provided in every personalized and effective solution for your business. With a customer-centered approach and a highly skilled team, we are committed to the success of your company.",
+        webrand: "With the trust of more than 20 brands and organizations of all sizes.",
+        webranddes: "Not only do we help businesses transform through technology, but we also provide them with the necessary support to make a significant difference in the lives of their customers and the communities they serve.",
+        welieder: "LEADERSHIP",
+        weliedersub: "Committed to creating significant change",
+        weliederdes: "The SST team is comprised of highly skilled professionals with experience in a variety of disciplines, including infrastructure, software development, project management, and customer support.",
+        wedirector: "Board of Directors",
+        wecargo: "President",
+        wename: "Name and Surname",
+        wecargodes: "The SST team is comprised of highly skilled professionals with experience in a variety of disciplines, including infrastructure, software development, project management, and customer support."
+        
+      }
+    },
+    es: {
+      translation: {
+        home: 'Inicio',
+        services: 'Que hacemos?',
+        about: 'Quienes somos?',
+        blog: 'Tecnología Actual',
+        contact: 'Contacto',
+        language: 'Idioma',
+        es: 'Español',
+        en: 'Inglés',
+        herotitle: 'Construyendo el futuro de su empresa',
+        herodescription: 'Juntos, transformaremos su visión de futuro en realidad aprovechando los beneficios del mundo digital',
+        herobutton:'Descubre',
+        dotitle:'¿Que hacemos?',
+        dosubtitle:'Transformamos su empresa a través de la tecnología',
+        dodescription:'Nuestros servicios permiten afrontar el reto de la transformación digital, desde el diseño y planeamiento del negocio hasta la consolidación del crecimiento de las ventas, reducción de costos operativos y de almacenamiento.',
+        dobutton:'Todos nuestros servicios',
+        differencetitle:'NUESTRA DIFERENCIA',
+        d1:'Talento humano en Tecnología',
+        d2:'Software a medida',
+        d3:'Rapidez y precisión: Inteligencia Artificial',
+        d4:'Construya la infraestructura mas economica y confiable',
+        d5:'La seguridad de estar en las nubes',
+        d6:'Las redes unen al mundo en un solo lugar',
+        wetitle:'¿Quienes somos?',
+        wesubtitle:'Desarrollamos soluciones tecnológicas innovadoras a través de nuestro conocimiento y experiencia',
+        wedescription:'SST es una empresa especializada en servicios, consultoría y soluciones de negocio basadas en TI con mas de 10 años de experiencia en la industria, acompañando la transformación y el crecimiento de empresas lideres a través de la tecnología.',
+        webutton:'¡Ven y conócenos!',
+        btitle:'Mantente informado',
+        benlace1:'Desarrollo web',
+        btitle1:'La importancia del talento humano y el desarrollo del software',
+        benlace2:'Transformación digital',
+        btitle2:'La importancia de la transformación digital en las pymes',
+        benlace3:'Inteligencia de negocios',
+        btitle3:'Cómo las soluciones de inteligencia de negocios están ayudando a las empresas a tomar decisiones informadas',
+        fdescription:'Somos eficientes y capaces de enfrentar retos tecnológicos, brindando un servicio integral de calidad',
+        flinks:'Soluciones',
+        flink1:'Infraestructura',
+        flink2:'Software',
+        flink3:'Consultoría',
+        flink4:'Marketing digital',
+        fcontact:'Contacto',
+        fmail:'Correos:',
+        fphone:'Celular:',
+        fsub:'Mantente informado',
+        fsubdescription:'Regístrese para recibir nuestro boletín informativo por correo electrónico y recibir las notas más recientes directamente en su bandeja de entrada.',
+        fsubbutton:'Inscribirse',
+        contacthero:'¡Únete a la transformación y marca la diferencia hoy!',
+        cspan1:'Llámanos',
+        cdes1:'Estamos aquí para ayudarte en todo momento.',
+        cspan2:'Ubícanos',
+        cdes2:'Ver ubicación',
+        cspan3:'Envíanos un correo',
+        cdes3:'Nuestro equipo de expertos está a tu disposición.',
+        formtitle:'Ponte en contacto',
+        formsub:'Habla con un experto',
+        formbutton:'¡Enviar!',
+        csocials:'Síguenos en nuestras redes sociales',
+        dotitle2:'La innovación, el motor del futuro',
+        dodes2:'En SST, la confianza es esencial para el éxito. Ofrecemos soluciones tecnológicas integrales y construimos relaciones de confianza con nuestros clientes y sus comunidades para lograr un impacto positivo en sus negocios y en el mundo.',
+        servicestitle:'Servicios, tecnológicos para su empresa',
+        productstitle:'Productos. Soluciones para llegar al siguiente nivel',
+        erpcomercial:'Trace ERP Comercial',
+        comercialdes:'Optimiza la gestión de tu empresa con Trace ERP, el sistema informático integrado que simplifica y unifica todas las áreas de tu negocio en una sola plataforma. Toma decisiones informadas y eficaces, optimiza tus procesos y lleva tus operaciones al siguiente nivel.',
+        productsbutton:'Ver más',
+        erpindustrial:'Trace ERP Industrial',
+        industrialdes:'Trace ERP Industrial es la solución informática de gestión y planificación de recursos empresariales de SST diseñada para empresas del sector industrial. Con ella, podrás integrar todas las áreas de tu negocio en un solo sistema, lo que te permitirá mejorar la eficiencia, reducir los costos y tomar decisiones más informadas.',
+        erphotel:'Trace ERP Hoteles',
+        hoteldes:'Optimiza la gestión de tu empresa con Trace ERP, el sistema informático integrado que simplifica y unifica todas las áreas de tu negocio en una sola plataforma. Toma decisiones informadas y eficaces, optimiza tus procesos y lleva tus operaciones al siguiente nivel.',
+        erprestaurant:'Trace ERP Restaurante',
+        restaurantdes:'Trace ERP Industrial es la solución informática de gestión y planificación de recursos empresariales de SST diseñada para empresas del sector industrial. Con ella, podrás integrar todas las áreas de tu negocio en un solo sistema, lo que te permitirá mejorar la eficiencia, reducir los costos y tomar decisiones más informadas.',
+        usuariotitle:'Confianza en acción',
+        uspan1:'Inteligencia Artificial',
+        topic1:'Automatiza las tareas con IA',
+        usuariodes:'Caso de estudio',
+        uspan2:'Internet de las cosas',
+        topic2:'Internet de las cosas: incorporar datos en el día a día',
+        uspan3:'Cableado estructurado',
+        topic3:'El cableado del Perú',
+        wespan:'Construyendo sobre la confianza',
+        wedesmain:'Somos una empresa responsable, generamos cambios positivos en beneficio a la sociedad',
+        wechar: "Confianza,"+"<br>"+
+                "Experiencia e"+"<br>"+
+                "Innovación",
+        wechardes: 'proporcionada en cada solución personalizada y efectiva para tu empresa. Con un enfoque centrado en el cliente y un equipo altamente capacitado, estamos comprometidos con el éxito de tu negocio.',
+        webrand:'Con la confianza de más de 20 marcas y organizaciones  de todos los tamaños.',
+        webranddes:'No solo ayudamos a las empresas a transformarse mediante la tecnología, sino que también les brindamos el apoyo necesario para marcar una diferencia significativa en las vidas de sus clientes y en las comunidades a las que sirven.',
+        welieder:'LIDERAZGO',
+        weliedersub:'Comprometidos a crear un cambio significativo',
+        weliederdes:'El equipo de SST está formado por profesionales altamente capacitados y con experiencia en una variedad de disciplinas, incluyendo infraestructura, desarrollo de software, gestión de proyectos y soporte al cliente.',
+        wedirector:'Junta Directiva',
+        wecargo:'Presidente',
+        wename:'Nombre y Apellido',
+        wecargodes:'El equipo de SST está formado por profesionales altamente capacitados y con experiencia en una variedad de disciplinas, incluyendo infraestructura, desarrollo de software, gestión de proyectos y soporte al cliente.'
+       
+      }
+    }
+  };
+
+
+
+  i18next.init({
+    lng: 'es',
+    resources: resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false // permite la interpolación de HTML
+    }
+  }, 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  function(err, t) {
+    if (err) return console.error('Error al cargar los recursos:', err);
+    
+    // Inicializa el contenido traducido en el idioma seleccionado
+    updateLanguage();
+  });
+  
+  function updateLanguage(element) {
+    // Obtener el idioma del elemento clickeado o el idioma actual del navegador
+    const lang = element ? element.getAttribute('data-lang') : window.navigator.language || 'es';
+  
+    // Cambiar el idioma actual de i18next
+    i18next.changeLanguage(lang, function(err, t) {
+      if (err) return console.error('Error al cambiar el idioma:', err);
+      
+      // Actualizar todos los elementos con la propiedad data-i18n
+      document.querySelectorAll('[data-i18n]').forEach(function(element) {
+        element.innerHTML = t(element.getAttribute('data-i18n'));
+      });
+  
+      // Actualizar el idioma del texto "ES" o "EN" en la barra de idiomas
+      document.querySelector('.idiom-change').innerHTML = lang.toUpperCase();
+      
+      console.log('Idioma actualizado a:', lang);
+    });
+  }
+  
+  // Agregar un listener de click a cada enlace de idioma
+  document.querySelectorAll('.dropdown-menu a').forEach(function(element) {
+    element.addEventListener('click', function(event) {
+      event.preventDefault();
+      updateLanguage(this);
+    });
+  });
+   
+ 
+
+  
+  
+
+/*   function changeLanguage(lang) {
+    $("[data-i18n]").each(function () {
+      $(this).html(translations[$(this).data("i18n")][lang]);
+    });
+    // actualiza los placeholders de los inputs
+    $("#name").attr("placeholder", translations["formname"][lang]);
+    $("#company").attr("placeholder", translations["formcompany"][lang]);
+    $("#email").attr("placeholder", translations["formemail"][lang]);
+    $("#phone").attr("placeholder", translations["formphone"][lang]);
+    $("#message").attr("placeholder", translations["formmessage"][lang]);
+    // actualiza el texto del formulario
+    $("#formtitle").text(translations["formtitle"][lang]);
+    $("#formsubtitle").text(translations["formsub"][lang]);
+    $("#formbutton").text(translations["formbutton"][lang]);
+    $("#csocials").text(translations["csocials"][lang]);
+  }
+   */
